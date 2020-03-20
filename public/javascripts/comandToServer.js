5e06fae729745ff0185b1e0f3700a5008af537ba
Ygor <ygor_sy@hotmail.com>
2020-03-20 17:29:09 -0300

    Removendo Classes genéricas e ajustando as funções

--- public/javascripts/comandToServer.js
+++ public/javascripts/comandToServer.js
@@ -1,27 +1,15 @@
 function toggleAction(buttonType) {
     let sendData;
-    //let element = document.getElementById("img");
-    let element = $("#img").src;
     if(buttonType == 'on')
     {
-        element = '/images/acesso.png';
-        sendData = 1;
+      $('#image').attr("src",'/images/acesso.png');
+      sendData = 1;
     }
     else
     {
-        element = '/images/apagado.png';
-        sendData = 0;
+      $('#image').attr("src",'/images/apagado.png');
+      sendData = 0;
     }
-    console.log(sendData);
-    $.post("/led", {sendData : sendData});
-    /*axios.post('/led', {
-        sendData : sendData,
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });*/
-      
+    let response = $.post("/led", {sendData : sendData});
+    console.log(response);
 }
